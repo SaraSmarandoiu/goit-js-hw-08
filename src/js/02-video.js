2f0779b26738058ad6a9d106429bb0c60fc4479d
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-04-21 22:04:06 +0300

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Vimeo from '@vimeo/player';
 
+const player = new Vimeo('vimeo-player');
+
+player.on('timeupdate', function (event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+});
+import throttle from 'lodash.throttle';
+
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    const currentTime = event.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
 
+function saveToLocalStorage() {
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
+    const formData = { email, message };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }
+  
+  
+  function fillFormFields() {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+      const { email, message } = JSON.parse(savedData);
+      document.querySelector('input[name="email"]').value = email;
+      document.querySelector('textarea[name="message"]').value = message;
+    }
+  }
+  
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
+    const formData = { email, message };
+    localStorage.removeItem('feedback-form-state');
+    console.log(formData);
+  }
+  
+  
+  document.querySelectorAll('.feedback-form input, .feedback-form textarea').forEach(element => {
+    element.addEventListener('input', _.throttle(saveToLocalStorage, 500));
+  });
+  
+  window.addEventListener('load', fillFormFields);
+  
+  document.querySelector('.feedback-form').addEventListener('submit', handleSubmit);
+  
+  
